4db433958824c2397d509b0b6ddebc0a792796c5
Charles Desneuf <c.desneuf@gmail.com>
2012-12-02 02:37:59 +0100

    Add log messages for debug

--- marvin.js
+++ marvin.js
@@ -12,7 +12,19 @@
 	 * This is Marvin !
 	 */
 	 var Marvin = (function(){
-	 	// This is private 
+
+	 	/**
+	 	 * If this var is set to true we will display debug message
+	 	 * @type {Boolean}
+	 	 */
+	 	var debug = false;
+
+	 	/**
+	 	 * Bind a module name to its newOrder function
+	 	 * Key is moduleName
+	 	 * Value is the function
+	 	 * @type {Object}
+	 	 */
 	 	var orderReceivers = {};
 
 	 	// This is public
@@ -22,25 +34,41 @@
 	 			// Store Marvin instance
 	 			var Marvin = this;
 
-	 			console.log('Starting Marvin');
-	 			// Just to have an example of optimist usage
-	 			if(argv.debug){
-	 				console.log('Debug Mode');
-	 			}
+	 			// Store debug
+	 			debug = argv.debug;
+
+	 			if(debug)
+	 				console.log('Starting Marvin');
 
 	 			// Start all modules
 	 			findit.find('../modules').on('file', function(file, stat){
 	 				//If its file is start.js we need it. This should start module
 					if(file.endsWith('start.js')){
-						require(file).start(Marvin);
+						var module = require(file);
+						module.start(Marvin);
+
+						if(debug)
+							console.log(module.moduleName + ' started');
 					}
 				});
 
 	 		},
 
 	 		"addOrderReceiver": function(receiver){
-	 			console.log('Adding order receiver for ' + receiver.moduleName);
-	 			orderReceivers[receiver.moduleName] = receiver.newOrder;
+	 			if(debug)
+	 				console.log('Adding order receiver for ' + receiver.moduleName);
+
+	 			if(typeof receiver.moduleName === 'string' && typeof receiver.newOrder === 'function'){
+	 				orderReceivers[receiver.moduleName] = receiver.newOrder;
+	 			}
+	 			else{
+	 				var message = 'Can\'t add order receiver';
+	 				if(typeof receiver.moduleName === 'string'){
+	 					message += ' for module ' + receiver.moduleName;
+	 				}
+	 				console.log(message);
+	 			}
+	 			
 	 		},
 
 	 		"newOrder": function(order){
@@ -51,6 +79,13 @@
 				// Get corresponding receiver and pass it the order
 				if(typeof orderReceivers[module] === 'function'){
 					orderReceivers[module](params);	
+				}
+				else {
+					if(debug){
+						console.log('Can\'t find module : ' + module);
+						console.log('Parameters were :');
+						console.log(params);
+					}
 				}
 					
 	 		}
